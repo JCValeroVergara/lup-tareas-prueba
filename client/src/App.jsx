9e87ff05394d177ee5ce077244647bab26673ab5
Juank_Valero <jvalero2009@gmail.com>
2025-01-28 15:29:31 -0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+export function App() {
+
+    return (
+        <>
+            <div className='flex flex-col items-center justify-center h-screen'>
+                <h1 className='text-5xl font-bold text-center'> App Tareas</h1>
+            </div>
+        </>
+    )
+}
+
