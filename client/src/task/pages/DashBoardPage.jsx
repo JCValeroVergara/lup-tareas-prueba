744d896efa6c5c9a23ef03dd0a467c74d89bc9f4
Juank_Valero <jvalero2009@gmail.com>
2025-01-29 19:33:31 -0500

    Fix, método getTaskByUser, método Delete en borrado lógico

--- client/src/task/pages/DashBoardPage.jsx
+++ client/src/task/pages/DashBoardPage.jsx
@@ -1,10 +1,13 @@
+
 import { TaskLayout } from '../layout';
 
 export const DashBoardPage = () => {
     return (
         <>
             <TaskLayout>
-                <h1>DashBoardPage</h1>
+                <div>
+                    hello dash board
+                </div>
             </TaskLayout>
         </>
     );
